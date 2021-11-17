48e1634efa9fb9c202bde434f44cbbd986de99c1
Hemanth-Gattu <hemanth.3@iitj.ac.in>
2021-11-18 03:41:46 +0530

    first

---
+++ src/components/Transactionlist.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+import { Transaction } from './Transaction';
+export const TransactionsList = () => {
+    const {transactions} = useContext(GlobalContext);
+
+    return (
+        <>
+           <h3>Transactions</h3> 
+           <ul className="list">
+           {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+      </ul>
+        </>
+    )
+}
